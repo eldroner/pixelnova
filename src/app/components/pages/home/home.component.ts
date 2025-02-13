98f3dcaa0cf8edbb9fc64f3240aa7c26b27cf5d2
Jose <amadowebs@gmail.com>
2025-02-14 00:08:09 +0100

    mejora de página home mas ancha y enlaces del sidebar

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "../../shared/hero/hero.component";
 import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
 import { ServiceListComponent } from "../../shared/service-list/service-list.component";
+import { SpacerComponent } from "../../shared/spacer/spacer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, SidebarComponent, ServiceListComponent],
+  imports: [HeroComponent, SidebarComponent, ServiceListComponent, SpacerComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
